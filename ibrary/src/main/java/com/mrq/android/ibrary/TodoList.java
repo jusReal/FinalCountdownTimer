7f97b2b25d639563a9481ed7b7c9a841a7a25bef
Mrq <mrq_android@163.com>
2018-02-01 00:08:22 +0800

    添加待办事项todo list

---
+++ ibrary/src/main/java/com/mrq/android/ibrary/TodoList.java
@@ -0,0 +1,16 @@
+package com.mrq.android.ibrary;
+
+/**
+ * 项目名称：FinalCountdownTimer
+ * 类描述：
+ * 创建人：Just
+ * 创建时间：2018/2/1 0:06
+ */
+public class TodoList {
+
+    /**
+     * todo : 18/2/1 输出格式 例如: 2:30  带时间格式开关
+     *
+     *
+     */
+}
